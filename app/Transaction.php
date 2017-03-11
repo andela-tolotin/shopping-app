a53608aafd713343c42df7abc674328daecb55f2
Temitope Olotin <temitope.olotin@andela.com>
2017-03-11 15:05:21 +0100

    map user to transactions

--- app/Transaction.php
+++ app/Transaction.php
@@ -23,4 +23,9 @@ class Transaction extends Model
     {
     	return $this->belongsTo('App/Product');
     }
+
+    public function user()
+    {
+    	return $this->belongsTo('App/User');
+    }
 }
