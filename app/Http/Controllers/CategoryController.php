3e02350b068ae2f75c6c692840f9b01812aee811
Ademola <ademola.raimi@andela.com>
2017-03-19 17:22:47 +0100

    add product form and category form

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Cloudder;
+use App\Product;
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Requests\CategoryRequest;
+
+class CategoryController extends Controller
+{
+    /**
+     * Displays category form
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showCategoryForm()
+    {
+        return view('category.form');
+    }
+
+    /**
+     * Save category to db
+     *
+     * @param CategoryRequest $request
+     */
+    public function postCategory(CategoryRequest $request)
+    {
+        $category = Category::create([
+            'name' => $request['name'],
+            'description' => $request['description'],
+        ]);
+
+        if ($category) {
+            return redirect('/');
+        }
+
+        return redirect()->back();
+    }
+}
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Cloudder;
+use App\User;
+use App\Product;
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest;
+
+class ProductController extends Controller
+{
+    /**
+     * Displays product form
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showProductForm()
+    {
+        $categories = Category::all();
+        return view('product.form', compact('categories'));
+    }
+
+    /**
+     * Add product to db
+     *
+     * @param ProductRequest $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function postProduct(ProductRequest $request)
+    {
+        $product = Product::create([
+            'name'            => $request['name'],
+            'description'     => $request['description'],
+            'category_id'     => $request['category'],
+            'price'           => $request['price'],
+            'discount'        => $request['discount'],
+            'tax'             => $request['tax'],
+            'product_img_url' => $request['photo'] == '' ? '': $this->uploadProductImage($request),
+        ]);
+
+        if ($product) {
+            return redirect('/');
+        }
+
+        return redirect()->back();
+    }
+
+    /**
+     * Upload product image to cloudinary
+     *
+     * @param $request
+     * @return mixed
+     */
+    public function uploadProductImage($request)
+    {
+        $productImage = $request->file('photo');
+
+        Cloudder::upload($productImage, null, [
+            'format' => 'jpg',
+            'crop'   => 'fill',
+            'width'  => 250,
+            'height' => 250,
+        ]);
+
+        return  Cloudder::getResult()['url'];
+    }
+}
