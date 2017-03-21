17b697416dd79ac3f8be4b8a4ae8491405d310f7
Temitope Olotin <temitope.olotin@andela.com>
2017-03-21 23:36:14 +0100

    fix product edit page

--- resources/views/dashboard/product/edit_form.blade.php
+++ resources/views/dashboard/product/edit_form.blade.php
@@ -17,16 +17,24 @@
             @endif
             <form id="register-form4" method="post" action="/product/{{ $product->id }}/update" enctype="multipart/form-data">
                 {{ csrf_field() }}
+                <div class="profile-image">
+                    <div class="profile-image-data">
+                        <figure class="user-avatar medium">
+                            @if ($product->product_img_url == '')
+                                <img src="{{ asset('images/dashboard/profile-default-image.png') }}" alt="profile-default-image">
+                            @else
+                                <img src="{{ $product->product_img_url }}">
+                            @endif
+                        </figure>
+                        <p class="text-header">Product Photo</p>
+                        <p class="upload-details"><input type="file" class="" name="photo"></p>
+                    </div>
+                </div>
                 <div class="input-container">
                     <label for="name" class="rl-label required">Name</label>
                     <input type="text" id="name" name="name" value="{{ $product->name }}" required="required">
                 </div>
-                <div class="input-container">
-                    <label for="description" class="rl-label required">Description</label>
-                    <input type="text" id="description" name="description" value="{{ $product->description }}" required="required">
-                </div>
                 <div>
-
                     <label for="category">Product Category</label>
 
                     <select class = "form-control" name="category">
@@ -41,11 +49,11 @@
                     </select>
 
                 </div>
-                <div class="input-container">
+                <div class="input-container half">
                     <label for="price" class="rl-label required">Price</label>
                     <input type="number" id="price" name="price" value="{{ $product->price }}" required="required">
                 </div>
-                <div class="input-container">
+                <div class="input-container half">
                     <label for="discount" class="rl-label required">Discount</label>
                     <input type="number" id="discount" name="discount" value="{{ $product->discount }}">
                 </div>
@@ -53,19 +61,10 @@
                     <label for="tax" class="rl-label required">Tax</label>
                     <input type="number" id="tax" name="tax" value="{{ $product->tax }}">
                 </div>
-                <div class="profile-image">
-                    <div class="profile-image-data">
-                        <figure class="user-avatar medium">
-                            @if ($product->product_img_url == '')
-                                <img src="images/dashboard/profile-default-image.png" alt="profile-default-image">
-                            @else
-                                <img src="{{ $product->product_img_url }}">
-                            @endif
-                        </figure>
-                        <p class="text-header">Product Photo</p>
-                        <p class="upload-details">Minimum size 70x70px</p>
-                    </div>
-                    <input type="file" class="" name="photo">
+
+                 <div class="input-container">
+                    <label for="description" class="rl-label required">Description</label>
+                    <textares  name="description"  required="required">{{ $product->description }}</textares>
                 </div>
                 <button type="submit" class="button mid dark">Update Product</button>
             </form>
