ed5479fc73181b31d10c8ba397d3c851dd5684ab
Temitope Olotin <temitope.olotin@andela.com>
2017-03-30 06:46:12 +0100

    add migration file

--- app/Http/Controllers/AdvertController.php
+++ app/Http/Controllers/AdvertController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use Cloudder;
+use App\Advert;
 use Illuminate\Http\Request;
 use App\Http\Requests\AdvertRequest;
 
@@ -14,6 +17,37 @@ class AdvertController extends Controller
 
     public function uploadAdvert(Request $request)
     {
+    	if (request->has('photo')) {
 
+    	}
+    }
+
+    /**
+     * Upload product image to cloudinary
+     *
+     * @param $request
+     * @return mixed
+     */
+    public function uploadProductImage($request)
+    {
+        $cloudinaryWrapper = [];
+        $urls = [];
+
+        foreach ($request['images'] as $key => $image) {
+            $result = Cloudder::upload($image, null, [
+                'format' => 'jpg',
+                'crop'   => 'fill',
+                'width'  => 1280,
+                'height' => 768,
+            ]);
+
+            array_push($cloudinaryWrapper, $result);
+        }
+
+        foreach ($cloudinaryWrapper as $key => $value) {
+            array_push($urls, $value->getResult()['url']);
+        }
+
+        return json_encode($urls);
     }
 }
