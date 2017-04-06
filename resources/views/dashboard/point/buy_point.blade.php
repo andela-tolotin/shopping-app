080a3ced0e4c3429b44661296b9c7724a39957a6
Temitope Olotin <temitope.olotin@andela.com>
2017-04-06 20:27:43 +0100

    buy point with stripe

--- resources/views/dashboard/point/buy_point.blade.php
+++ resources/views/dashboard/point/buy_point.blade.php
@@ -23,15 +23,14 @@
                 <label for="name" class="rl-label required">Amount</label>
                 <input type="text" id="name" name="name" placeholder="Enter name of amount to buy here" required="required">
             </div>
-            <script src="https://checkout.stripe.com/checkout.js" class="stripe-button" data-key="{{ $paymentGateway->client_id }}" data-amount="{{ $product->price * 1000 }}" data-name="{{ $product->name }}" data-description="Payments" data-image="{{ $paymentGateway->logo }}" data-locale="auto" data-currency="krw"
+            <script src="https://checkout.stripe.com/checkout.js" class="stripe-button" data-key="{{ $paymentGateway->client_id }}" data-amount="" data-name="Buy Point" data-description="Payments" data-image="{{ $paymentGateway->logo }}" data-locale="auto" data-currency="krw"
             @if (Auth::check())
             data-email="{{ Auth::user()->email }}"
             @endif
             >
             </script>
             {{ csrf_field() }}
-            <input type="hidden" name="amount" value="{{ $product->price * 1000 }}" />
-            <input type="hidden" name="product_id" value="{{ $product->id }}" />
+            <input type="hidden" name="amount" value="" />
             <input type="hidden" name="payment_gateway_id" value="{{ $paymentGateway->id }}" />
         </form>
         @endif
