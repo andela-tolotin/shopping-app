0ecea31f863e838817b27085d7dc9486459865c3
Ademola <ademola.raimi@andela.com>
2017-04-09 21:43:22 +0100

    add permission

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -20,25 +20,26 @@
 				<!-- /GRAPH STATS LIST ITEM -->
 
 				<!-- GRAPH STATS LIST ITEM -->
-				<div class="graph-stats-list-item violet line">
-					<a href="user/orders"><p class="text-header">Total Orders</p></a>
+				<a href="user/orders"><div class="graph-stats-list-item violet line">
+					<p class="text-header">Total Orders</p>
 					<h2>{{ $userOrders }}</h2>
-				</div>
+				</div></a>
 				<!-- /GRAPH STATS LIST ITEM -->
-
+				@can ( 'ADMIN_MANAGER', Auth::user()->role_id )
 				<!-- GRAPH STATS LIST ITEM -->
-				<div class="graph-stats-list-item blue step">
-					<a href="/orders"><p class="text-header">Total Unapproved Orders</p></a>
+				<a href="/orders"><div class="graph-stats-list-item blue step">
+					<p class="text-header">Total Unapproved Orders</p>
 					<h2>{{ $totalUnapprovedOrder }}</h2>
-				</div>
+				</div></a>
+				@endcan
 				<!-- /GRAPH STATS LIST ITEM -->
-
+				@can ( 'ADMIN', Auth::user()->role_id )
 				<!-- GRAPH STATS LIST ITEM -->
-				<div class="graph-stats-list-item red curve">
-					<a href="/transactions"><p class="text-header">Total Amount of Transactions</p></a>
+				<a href="/transactions"><div class="graph-stats-list-item red curve">
+					<p class="text-header">Total Amount of Transactions</p>
 					<h2>{{ $totalTransactionAmount }}</h2>
-				</div>
-				<!-- /GRAPH STATS LIST ITEM -->
+				</div></a>
+				@endcan
 			</div>
 
         </div>
