0ee37801ad7e59169ba4f9c0d3d6dd486078d61c
Temitope Olotin <temitope.olotin@andela.com>
2017-04-25 22:47:19 +0100

    fix breaking homepage product listings

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,7 +27,9 @@ class HomeController extends Controller
         $userPointWallet = Auth::user()->pointWallet()->first();
         $userOrders = Auth::user()->orders;
 
-        $unapprovedOrders = Order::where('status', 0)->orderBy('created_at', 'DESC')->get();
+        $unapprovedOrders = Order::where('status', 0)
+            ->orderBy('created_at', 'DESC')
+            ->get();
 
         foreach ($unapprovedOrders as $index => $order) {
             if ($order->user_id == Auth::user()->id ) {
