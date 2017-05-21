e2782e9ef836a417a8d58d9a884c290c6dce95ad
Temitope Olotin <temitope.olotin@andela.com>
2017-05-21 21:52:15 +0100

    add user role

--- resources/views/dashboard/manage_user/list_users.blade.php
+++ resources/views/dashboard/manage_user/list_users.blade.php
@@ -26,7 +26,7 @@
 			<td>{{ $user->email }}</td>
 			<td>{{ $user->phone }}</td>
 			<td>{{ $user->gender }}</td>
-			<td>{{ ucwords($user->role()->name) }}</td>
+			<td>{{ ucwords($user->role->name) }}</td>
 			<td><a href="{{ route('edit_user', ['id' => $user->id]) }}" title="Edit {{ $user->name }}"> <i class="glyphicon glyphicon-pencil"></i> Edit </a></td>
 			<td><a class="delete-user" href="{{ route('delete_user', ['id' => $user->id]) }}" title="Delete {{ $user->name }}"> <i class="glyphicon glyphicon-trash Delete"></i> Delete</a></td>
 		</tr>
