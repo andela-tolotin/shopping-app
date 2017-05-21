21a41f563cd658003c2451e01ef41ccc7bd43e5e
Temitope Olotin <temitope.olotin@andela.com>
2017-05-21 23:07:14 +0100

    get product service manager

--- resources/views/dashboard/manage_user/edit_user.blade.php
+++ resources/views/dashboard/manage_user/edit_user.blade.php
@@ -121,7 +121,7 @@
             <div class="input-container half">
                 <label for="product" class="rl-label required">Product</label>
                 <label for="gender" class="select-block">
-                {{ dump($product[0]->serviceManager) }}
+                {{ dump($products[0]->serviceManager) }}
                     <select name="product">
                         <option value="">Product</option>
                         @foreach($products as $product)
